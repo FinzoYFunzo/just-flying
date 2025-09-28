2e3151e22f66a60959aa4b59f6c6f24b2e764fc6
FinzoYFunzo <joaquinandresparada@gmail.com>
2025-09-28 18:13:12 -0300

    fix: form bug
    el formulario no almacenaba el valor actual del tipo de busqueda que se
    estaba realizando

--- src/components/Controller.tsx
+++ src/components/Controller.tsx
@@ -1,53 +1,69 @@
-import { appConfig, searchOptions, ApiArray2String, ApiString2Array} from '../helpers/utils.js'
+import { useEffect, useState } from 'react';
+import { appConfig, searchOptions, ApiArray2String, ApiString2Array } from '../helpers/utils.js'
 import './Controller.css'
 
-function Controller({callback}: {callback: any}){
-    const defaultQueryType = searchOptions.get('search_by');
-    const defaultSearch = searchOptions.get(defaultQueryType);
-    const defaultApiKeys = ApiArray2String(appConfig.get('API_keys'));
-    const defaultInterval = appConfig.get('interval');
-    
-    function updateConfig (formData: FormData) {
-        const queryType: string = (formData.get('queryType') as string) || ''
-        searchOptions.set("search_by", queryType, true);
-        searchOptions.set(queryType, formData.get('search'), true);
-    
-        const apiKeysArray = ApiString2Array(formData.get('apiKeys') as string)
+function Controller({ callback }: { callback: any }) {
+    const [queryType, setQueryType] = useState<string>("");
+    const [searchQuery, setSearchQuery] = useState<string>("");
+    const [apiKeys, setApiKeys] = useState<string>("");
+    const [interval, setInterval] = useState<number>();
 
+    useEffect(() => {
+        setQueryType(searchOptions.get('search_by'))
+        setSearchQuery(searchOptions.get(queryType));
+        setApiKeys(ApiArray2String(appConfig.get('API_keys')));
+        setInterval(appConfig.get('interval'));
+    }, [])
+    
+
+    function updateConfig(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const queryType: string = (formData.get('queryType') as string) || ''
+        const searchQuery: string = (formData.get('search') as string) || ''
+        const apiKeys: string = (formData.get('apiKeys') as string)
+        const apiKeysArray = ApiString2Array(apiKeys)
+        const intervalValue = Number(formData.get('interval'));
+
+        setQueryType(queryType);
+        setSearchQuery(searchQuery);
+        setApiKeys(apiKeys);
+
+        searchOptions.set("search_by", queryType, true);
+        searchOptions.set(queryType, searchQuery, true);
         appConfig.set('API_keys', apiKeysArray, true);
 
-        console.log(formData.get('interval'));
-
-        const intervalValue = Number(formData.get('interval'));
-        if (Number.isNaN(intervalValue) || intervalValue < 10){
+        if (Number.isNaN(intervalValue) || intervalValue < 10) {
+            setInterval(10);
             appConfig.set('interval', 10, true) // return default config on error
         }
         else {
+            setInterval(intervalValue)
             appConfig.set('interval', intervalValue, true);
         }
 
         callback();
     }
 
-    return(
-        <form id='controller' action={updateConfig}>
+    return (
+        <form id='controller' onSubmit={updateConfig}>
             <fieldset>
                 <legend><b>Controller</b></legend>
 
                 <span>Search Options <a className='tooltip'>?</a></span>
                 <div className='field'>
                     <label>Select your query type:</label>
-                    <select defaultValue={defaultQueryType} name='queryType'>
+                    <select value={queryType} name='queryType' onChange={e => setQueryType(e.target.value)}>
                         <option value='query'>query</option>
                         <option value='topics'>topics</option>
-                        <option value='collections'>colections</option>
+                        <option value='collections'>collections</option>
                     </select>
                 </div>
 
                 <div className='field'>
                     <label>What to search?</label>
                     <input
-                        defaultValue={defaultSearch}
+                        defaultValue={searchQuery}
                         name='search'
                         type='text'
                     />
@@ -57,13 +73,13 @@ function Controller({callback}: {callback: any}){
                 <div className='field'>
                     <label>API Keys from Unsplash <br></br><span className='muted'>separated by coma (,)</span></label>
                     <input
-                        defaultValue={defaultApiKeys}
+                        defaultValue={apiKeys}
                         name='apiKeys'
                         type='text'
                     />
                     <label>Interval <span className='muted'>un seconds</span> </label>
                     <input
-                        defaultValue={defaultInterval}
+                        defaultValue={interval}
                         name='interval'
                         type='text'
                     />
