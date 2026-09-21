ec9b82f7da83e76eeb63723331dca1ae08652faf
FinzoYFunzo <joaquinandresparada@gmail.com>
2026-09-21 14:40:01 -0300

    feat: media-player

--- src/components/ImageLoader.tsx
+++ src/components/ImageLoader.tsx
@@ -6,8 +6,12 @@ import * as unsplash from '../helpers/unsplash-api.js'
 import { Image } from './Image.js'
 import Controller from './Controller.js'
 import store from 'store2'
+import { MusicPlayer } from "./MusicPlayer.js"
 
-// restara los valores por defecto en cada sesion
+const API_PLACEHOLDER = import.meta.env.VITE_API_PLACEHOLDER === "true";
+const URL_PLACEHOLDER = ["https://images.unsplash.com/photo-1563216368-5b6a40648062?crop=entropy&cs=srgb&fm=jpg&ixid=M3w3Mjk4NTF8MHwxfHJhbmRvbXx8fHx8fHx8fDE3ODkxNDg1NzB8&ixlib=rb-4.1.0&q=85", "https://images.unsplash.com/photo-1777979513826-bf31dc7ca628?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"]
+
+// WARN: restara los valores por defecto en cada sesion
 // eliminar esta linea para mantener persistencia
 if (!store.size()) utils.defaultConfig();
 let apiIndex: number = 0;
@@ -27,9 +31,19 @@ function ImageLoader() {
 
 
   // Cambia la imagen con id imageId
-  function changeImage(imageId: number): Promise<void> {
+  // WARN: la funcion es `async` solo por el PLACEHOLDER, no es necesario de otro modo
+  async function changeImage(imageId: number): Promise<void> {
     const options: Record<string, string> = utils.parseOptions() || {}; // leemos localStorage
     ({ unsplashSingleApi, apiIndex } = unsplash.getUnsplash(unsplashApis, apiIndex)); // obtenemos una api de la lista
+
+    if (API_PLACEHOLDER) {
+      setImageSrc(prev => {
+        const newSrc: Array<string> = [...prev];
+        newSrc[imageId] = URL_PLACEHOLDER[imageId]
+        return newSrc;
+      });
+      return
+    }
 
     return unsplash.fetchImage(unsplashSingleApi, options)
       .then(async (res: any) => {
@@ -104,6 +118,7 @@ function ImageLoader() {
         src={imageSrc[1]}
       />
       <Controller callback={touch} loading={loading} />
+      <MusicPlayer />
       <div className='preload-screen' style={{ display: (imageSrc[0] != undefined && imageSrc[1] != undefined) ? "none" : "inherit" }}>
         <p>Loading...</p>
         <p>Make sure to introduce your unsplash API keys</p>
